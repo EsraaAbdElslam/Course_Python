d6212360e76aea176d0e0c748f3d13210b8ff5bc
EsraaAbdElslam <eslamklanton@gmail.com>
2022-10-30 09:12:42 +0200

    EX01_14

---
+++ Turtle_draw_a_triangle.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.showturtle()
+#دي الطريقة الاولي
+# بس الاسهم مش بتلمس بعض
+turtle.right(45)
+turtle.forward(50)
+turtle.right(135)
+turtle.forward(75)
+turtle.right(135)
+turtle.forward(55)
+
+# go to method  
+#turtle.right(45)
+#turtle.forward(50)
+#turtle.right(135)
+#turtle.forward(75)
+#turtle.goto(0, 0)
+turtle.done()

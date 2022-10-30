8edb5c3a1db80e81bde25d22c23bfdc93c37aeaf
EsraaAbdElslam <eslamklanton@gmail.com>
2022-10-30 09:09:22 +0200

    EX01_12

---
+++ Turtle_draw_a_cross.py
@@ -0,0 +1,22 @@
+import turtle
+turtle.showturtle
+turtle.forward(50)
+turtle.forward(-100)
+#هو متعرفش علي اللون الابيض
+#turtle.color(white)
+#turtle.write(45)
+#turtle.color(black)
+#turtle.write(45)
+#turtle.forward(100)
+#الحل باستخدام رفع القلم وخفضه و goto
+#turtle.penup()
+#turtle.goto(0, 50)
+#turtle.pendown()
+#turtle.right(90)
+#turtle.forward(100)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(180)
+turtle.forward(100)
+turtle.done()

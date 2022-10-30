c07fe43deed867613e67072b9be9b65555d7c761
EsraaAbdElslam <eslamklanton@gmail.com>
2022-10-30 09:13:54 +0200

    EX01_15

---
+++ Turtle_draw_two_triangles.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.showturtle()
+#دي الطريقة الاولي
+# بس الاسهم مش بتلمس بعض
+#turtle.right(45)
+#turtle.forward(50)
+#turtle.right(135)
+#turtle.forward(75)
+#turtle.right(135)
+#turtle.forward(105)
+#turtle.left(135)
+#turtle.forward(75)
+#turtle.left(135)
+#turtle.forward(50)
+
+# go to method  
+turtle.right(45)
+turtle.forward(50)
+turtle.right(135)
+turtle.forward(75)
+turtle.right(135)
+turtle.forward(105)
+turtle.left(135)
+turtle.forward(75)
+turtle.left(135)
+turtle.goto(0, 0)
+turtle.done()

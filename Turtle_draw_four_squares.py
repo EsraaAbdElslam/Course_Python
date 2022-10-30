32ee18687de228809deefbd6f0be3449423c530f
EsraaAbdElslam <eslamklanton@gmail.com>
2022-10-30 09:08:17 +0200

    EX01_11

---
+++ Turtle_draw_four_squares.py
@@ -0,0 +1,40 @@
+import turtle
+turtle.showturtle()
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+
+#turtle.goto(0, 50)
+#turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+
+#turtle.forward(100)
+
+turtle.forward(50)
+turtle.right(90)
+
+#turtle.forward(50)
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+#ال goto مش محتاجه الخطوتين دول
+turtle.right(90)
+turtle.forward(50)
+turtle.done()
